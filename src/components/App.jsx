cfc4b923c9ffb69dded5cc734b63cf7d3029fcab
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-08-26 16:41:36 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
 import {
@@ -11,85 +11,80 @@ import {
 
 import './styles/styles.css';
 
-export class App extends Component {
-  state = {
-    picture: '',
-    page: 1,
-    pictureList: [],
-    loading: false,
-    icon: '',
-    modalShow: false,
-    totalHits: 0,
-  };
-  async componentDidUpdate(_, prevState) {
-    const prevPicture = prevState.picture;
-    const nextPicture = this.state.picture;
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
+export const App = () => {
+  const [picture, setPicture] = useState('');
+  const [page, setPage] = useState(1);
+  const [pictureList, setPictureList] = useState([]);
+  const [icon, setIcon] = useState('');
+  const [modalShow, setModalShow] = useState(false);
 
-    if (prevPicture !== nextPicture || prevPage !== nextPage) {
-      this.setState({ loading: true });
+  let loading = useRef(false);
+  let totalHitse = useRef(0);
+
+  useEffect(() => {
+    const lastPage = () => {
+      if (page * 12 > totalHitse.current) toast.error('Its last page');
+    };
+    const imageLoading = async () => {
+      loading.current = true;
+
       try {
         const { hits, totalHits, total } = await fetchArticlesWithQuery(
-          nextPicture,
-          this.state.page
+          picture,
+          page
         );
         if (total === 0) {
-          this.setState({ pictureList: null });
+          setPictureList(null);
           return toast.error('Image not found');
         }
-        this.setState(prevState => ({
-          totalHits,
-          pictureList: [...prevState.pictureList, ...hits],
-        }));
-        // перевірка лише на одну сторінку
+        totalHitse.current = totalHits;
+        setPictureList(prev => [...prev, ...hits]);
 
-        if (this.state.page * 12 > totalHits && total !== 0)
-          toast.error('Its last page');
         // перевірка знайшли щось чи ні
+        lastPage(); // перевірка лише на одну сторінку
       } catch (error) {
         toast.error('Sorry! We have some problem. Try again later! '); // помилка
         console.log(error.message);
       } finally {
-        this.setState({ loading: false });
+        loading.current = false;
       }
+    };
+    if (!picture) {
+      return;
     }
-  }
-  handleFormGetPicture = picture => {
-    this.setState({ picture, page: 1, pictureList: [] });
-  };
-  handleOnclick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-  handleGetIcon = url => {
-    this.setState(prevState => ({ modalShow: !prevState.modalShow }));
-    this.setState({ icon: url });
-  };
-  handleToggleModalShow = () => {
-    this.setState(prevState => ({ modalShow: !prevState.modalShow }));
-  };
-  lastPage = () => {
-    const { totalHits, page } = this.state;
-    if (page * 12 > totalHits) toast.error('Its last page');
-  };
-  render() {
-    const { pictureList, loading, modalShow, totalHits, page } = this.state;
+    imageLoading();
+  }, [picture, page]);
 
-    return (
-      <div className="App">
-        <Searchbar onSubmitGetPicture={this.handleFormGetPicture} />
-        <ImageGallery pictureList={pictureList} onClick={this.handleGetIcon} />
-        <Loader visible={loading} />
-        <ToastContainer autoClose={3000} />
-        {page * 12 < totalHits && (
-          <button className="Button" type="button" onClick={this.handleOnclick}>
-            Load more
-          </button>
-        )}
-        {modalShow && (
-          <Modal onClick={this.handleToggleModalShow} icon={this.state.icon} />
-        )}
-      </div>
-    );
-  }
-}
+  const handleFormGetPicture = picture => {
+    setPicture(picture);
+    setPage(1);
+    setPictureList([]);
+    totalHitse.current = 0;
+  };
+  const handleOnclick = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const handleGetIcon = url => {
+    setModalShow(prev => !prev);
+    setIcon(url);
+  };
+
+  const handleToggleModalShow = () => {
+    setModalShow(prev => !prev);
+  };
+  return (
+    <div className="App">
+      <Searchbar onSubmitGetPicture={handleFormGetPicture} />
+      <ImageGallery pictureList={pictureList} onClick={handleGetIcon} />
+      <Loader visible={loading.current} />
+      <ToastContainer autoClose={3000} />
+      {page * 12 < totalHitse.current && (
+        <button className="Button" type="button" onClick={handleOnclick}>
+          Load more
+        </button>
+      )}
+      {modalShow && <Modal onClick={handleToggleModalShow} icon={icon} />}
+    </div>
+  );
+};
